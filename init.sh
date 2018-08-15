7f4854e4013c040b8d27f67e8bfb2a14142533b4
SolalVall <solal.vallee@laposte.net>
2018-08-16 00:34:00 +0200

    Update init for macos based os

--- init.sh
+++ init.sh
@@ -35,33 +35,10 @@ else
 		echo -e "Your OS is not a Debian, RedHat or MacOs"
 		echo -e "Please Run the script with your package manager as argument"
 		echo -e "For exemple if you have a Arch Linux base OS run: ./init.sh pacman"
+		echo -e "\nNote if you are on MacOS please Install Brew and rerun init script"
 	else
 		echo -e "OK you use $other_os, let's GO !!"
 		./vim.sh "$other_os"
 	fi
 fi
  
-#Check your vim configuration
-#if [ -z $(which vim) ];
-#then
-#	#Install Vim
-#	package_manager vim
-#elif [ ! -d $vim_folder ];
-#then
-#	#Create structure for vim,pathogen and plugins (bundle)
-#	echo -e "Vim structure doesn't exist, let's install it....\n"
-#	mkdir -p ~/.vim/autoload ~/.vim/bundle
-#elif [ ! -f $pathogen ];
-#then
-#	#Download/Install pahtogen
-#	echo -e "Pathogen is not installed, start install....\n"
-#	curl -LSso $vim_folder/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-#elif [ ! -d $jedi_vim ];
-#then
-#	#Download/Install Jedi-vim
-#	echo -e "Jedi-vim is not installed, start install....\n"	
-#	git clone --recursive https://github.com/davidhalter/jedi-vim.git $vim_folder/bundle/jedi-vim
-#else
-#	echo -e "Your Vim Configuration is OK !\n"
-#fi
-#
